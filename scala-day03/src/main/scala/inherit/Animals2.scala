5d110b9ecf9bab52f2715be2b65fd3c321c6cac4
likaipeng <lkp1107440885@163.com>
2019-10-24 13:37:49 +0800

    Animals2 first commit

--- scala-day03/src/main/scala/inherit/Animals2.scala
+++ scala-day03/src/main/scala/inherit/Animals2.scala
@@ -1,7 +1,7 @@
 package inherit
 
 class Animals2 {
-  var name: String = ""
+  var name:String=""
 
   //定义一个方法
   def sleep() = println("动物在睡觉")
@@ -9,6 +9,10 @@ class Animals2 {
 
 //类继承动物类
 class Dog extends Animals2 {
+  def this(name:String){
+    this()
+    this.name=name
+  }
   def run: String = s"$name,在田野上愉快的奔跑！！！"
 
   //方法覆盖,在调用的时候会以子类中的方法为准
@@ -16,7 +20,7 @@ class Dog extends Animals2 {
 }
 
 object Dog {
-  def apply(name:String): Dog = new Dog()
+  def apply(name:String): Dog = new Dog(name:String)
 
   def main(args: Array[String]): Unit = {
 //    val dog = new Dog
@@ -24,6 +28,7 @@ object Dog {
 //    dog.sleep()
 //    println(dog.run)
     val dog = Dog("阿拉斯加")
+    println(dog.name)
     //判断类型是否兼容
     println(dog.isInstanceOf[Animals2])
     //类型转换，可以把一个类型转换成另一个类型
